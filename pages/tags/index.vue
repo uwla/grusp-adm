51a9e0916b57c05d9e9857d47ad4a8f7ff3d1df6
uwla <uwlalinux@gmail.com>
2023-06-12 10:24:08 -0300

    add very basic page that lists Grupo tags

---
+++ pages/tags/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <h1>TAGS</h1>
+        <ul>
+            <li v-for="tag,i in tags" :key="i">
+                {{ tag.name }}: {{ tag.tags }}
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+export default {
+    async asyncData({ $auth, $axios }) {
+        const token = $auth.strategy.token.get()
+        const headers = { 'Authorization' : token }
+        const tags = (await $axios.get('/tag', { headers })).data
+        return { tags }
+    },
+}
+</script>
