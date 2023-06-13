d4b42967cef14dc51c618f3d7256d764ca262e6b
uwla <uwlalinux@gmail.com>
2023-06-13 16:56:23 -0300

    add basic dashboard for Grupo tags

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -1,21 +1,33 @@
 <template>
     <main>
         <h1>TAGS</h1>
-        <ul>
-            <li v-for="tag,i in tags" :key="i">
-                {{ tag.name }}: {{ tag.tags }}
-            </li>
-        </ul>
+        <vue-data-table v-bind="{ columns, data: tags, lang: 'pt-br' }" />
     </main>
 </template>
 
 <script>
 export default {
+    middleware: ['auth'],
     async asyncData({ $auth, $axios }) {
         const token = $auth.strategy.token.get()
         const headers = { 'Authorization' : token }
         const tags = (await $axios.get('/tag', { headers })).data
         return { tags }
     },
+    data() {
+        return {
+            columns: [
+                {
+                    key: 'name',
+                    title: 'Nome',
+                },
+                {
+                    key: 'tags',
+                    title: 'Categoria',
+                    sortable: false,
+                },
+            ]
+        }
+    }
 }
 </script>
---
+++ plugins/vue-data-table.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import DataTable from '@andresouzaabreu/vue-data-table'
+import '@andresouzaabreu/vue-data-table/dist/DataTable.css'
+
+Vue.component('VueDataTable', DataTable);
+
