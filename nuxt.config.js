06a04942d8ec0d3535f3ddf7e912c45eaddc5f57
uwla <uwlalinux@gmail.com>
2023-08-12 16:20:51 -0300

    set better default head title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'grusp-adm',
+        title: 'GRUSP ADMIN',
         htmlAttrs: {
             lang: 'en',
         },
